017cd04eb702efd654250bb27bec0dc39dac0a3f
obieviscous <68691464+obieviscous@users.noreply.github.com>
2020-07-23 14:01:02 +0100

    Update email2sms.js

--- api/email2sms.js
+++ api/email2sms.js
@@ -27,6 +27,27 @@ module.exports = async (req, res) => {
     }).then(msg => {
         console.log(msg)
         res.status(200).send(msg.sid);
+        
+        
+        // Create Email
+        const email = {
+            to: fromAddress.address,
+            from: toAddress.address,
+            subject: `hello ${toAddress.local}`,
+            text: `${err}\n For email from ${fromAddress.address}`,
+        };
+        //Send Email
+        sgResp = sgMail.send(email)
+            .then(response => {
+                res.status(200).send("Sent Error Email");
+            })
+            .catch(error => {
+                res.status(500);
+            });
+        
+        
+        
+        
     }).catch(err => {
         console.log(err);
         //If we get an error when sending the SMS email the error message back to the sender
