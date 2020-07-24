5744111624a8d845e373bb5bb4c9c5dc03d00da4
obieviscous <68691464+obieviscous@users.noreply.github.com>
2020-07-24 13:03:44 +0100

    Update email2sms.js

--- api/email2sms.js
+++ api/email2sms.js
@@ -38,27 +38,28 @@ module.exports = async (req, res) => {
         let apResponse = JSON.parse(response.body);
      
         
+        
+         //Sending SMS with Twilio Client
+    client.messages.create({
+        to: `+19104151007`,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        body: `Message from:${body}`
+    }).then(msg => {
+      
+      
+        res.status(200).send(msg.sid);
+    }).catch(err => {
+        //If we get an error when sending the SMS email the error message back to the sender
+        res.status(500);
+
+      
+    });
+        
+        
      
         
         
-           sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        // Create Email
-        const email = {
-            to: fromAddress.address,
-            from: 'bot@digitalreceptionist.co.uk',
-            subject: apResponse.dialogue.current_task,
-            text: apResponse.response.says[0].speech,
-        };
-        //Send Email
-        sgResp = sgMail.send(email)
-            .then(response => {
-                res.status(200).send("Sent Error Email");
-            })
-            .catch(error => {
-                res.status(500);
-            });
-        
-         
+     
         
    
         
